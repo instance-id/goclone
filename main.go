d9de0f1930d9b6812364b9b84077560da4eeb680
instance.id <dan@instance.id>
2019-07-22 02:46:26 -0500

    Added ability to directly open repo in vscode/Jetbrains ide's (Android Studio, Goland, Rider, etc)

--- main.go
+++ main.go
@@ -7,13 +7,9 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/urfave/cli"
-)
-
-const (
-	Separator     = os.PathSeparator
-	ListSeparator = os.PathListSeparator
 )
 
 func main() {
@@ -23,6 +19,8 @@ func main() {
 		inputUrl  string
 		gpath     string
 		clonePath string
+		openApp   string
+		debug     bool
 	)
 
 	app := cli.NewApp()
@@ -41,6 +39,17 @@ func main() {
 			Value:       "",
 			Destination: &folder,
 		},
+		cli.StringFlag{
+			Name:        "open, o",
+			Usage:       "Open folder in application",
+			Value:       "none",
+			Destination: &openApp,
+		},
+		cli.BoolFlag{
+			Name:        "debug, d",
+			Usage:       "Debug flag to print data from input path",
+			Destination: &debug,
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
@@ -51,31 +60,55 @@ func main() {
 		if folder == "go" {
 			gpath = os.Getenv("GOPATH")
 			clonePath = fmt.Sprintf("%s/src/%s%s", gpath, u.Host, path.Dir(u.Path))
-			err := os.MkdirAll(clonePath, os.ModePerm)
-			if err != nil {
-				fmt.Printf("Error: %s", err)
-			}
+			if debug {
+				fullPath := strings.Replace(path.Base(u.Path), ".git", "", -1)
+				fmt.Printf("%s\n", fullPath)
+				fmt.Printf("%s/%s\n", clonePath, fullPath)
+			} else {
+				err := os.MkdirAll(clonePath, os.ModePerm)
+				if err != nil {
+					fmt.Printf("Error: %s", err)
+				}
 
-			doClone := fmt.Sprintf("cd %s && git clone %s", clonePath, inputUrl)
-			cmdCD := exec.Command("/bin/sh", "-c", doClone)
-			out, err := cmdCD.Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%s", out)
+				var doClone string
+				if openApp == "none" {
+					doClone = fmt.Sprintf("cd %s && git clone %s", clonePath, inputUrl)
+				} else {
+					fileName := strings.Replace(path.Base(u.Path), ".git", "", -1)
+					fullPath := fmt.Sprintf("%s/%s\n", clonePath, fileName)
+					doClone = fmt.Sprintf("cd %s && git clone %s && %s %s", clonePath, inputUrl, openApp, fullPath)
+				}
 
+				cmdCD := exec.Command("/bin/sh", "-c", doClone)
+				out, err := cmdCD.Output()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%s", out)
+			}
 		}
 		if folder == "git" {
 			gpath = os.Getenv("GITPATH")
 			clonePath = fmt.Sprintf("%s/", gpath)
-			doClone := fmt.Sprintf("cd %s && git clone %s", clonePath, inputUrl)
-			cmdCD := exec.Command("/bin/sh", "-c", doClone)
-			out, err := cmdCD.Output()
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Printf("%s", out)
+			if debug {
+				fmt.Printf(clonePath)
+			} else {
+				var doClone string
+				if openApp == "none" {
+					doClone = fmt.Sprintf("cd %s && git clone %s", clonePath, inputUrl)
+				} else {
+					fileName := strings.Replace(path.Base(u.Path), ".git", "", -1)
+					fullPath := fmt.Sprintf("%s/%s\n", clonePath, fileName)
+					doClone = fmt.Sprintf("cd %s && git clone %s && %s %s", clonePath, inputUrl, openApp, fullPath)
+				}
 
+				cmdCD := exec.Command("/bin/sh", "-c", doClone)
+				out, err := cmdCD.Output()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%s", out)
+			}
 		}
 		return nil
 	}
